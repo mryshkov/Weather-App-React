641d9a7b1d472efdc1e8c7d73c7a4072d34eba4c
Misha Krysha <3hotpinguin@gmail.com>
2025-08-25 01:18:25 +0300

    added working header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,66 @@
 
+import "./App.css";
+
 import Day from "./components/Day.tsx";
 import Hour from "./components/Hour.tsx";
 import Header from "./components/Header.tsx";
+import {useState} from "react";
 
-export default function App() {
+export default function App(){
+
+  const [daunclass, setdaunclass] = useState<string>();
+
   return (
     <>
       <Header />
-      <div className="days container">
-        <Day day="Monday"/>
-        <Day day="Tuesday"/>
-        <Day day="Wednesday"/>
-        <Day day="Thursday"/>
-        <Day day="Friday"/>
+      <div className="containerr">
+        <div className="days container">
+          <div id="daun" className={daunclass}>
+            <Day day="Monday"/>
+            <Day day="Tuesday"/>
+            <Day day="Wednesday"/>
+            <Day day="Thursday"/>
+            <Day day="Friday"/>
+          </div>
+        </div>
+        
+        <button className="btn-outline-primary" onClick={() => setdaunclass("visually-hidden")}>clickme</button>
+        <button className="btn-outline-primary" onClick={() => setdaunclass("")}>clickme</button>
+        <div className="hours container">
+          <Hour hour="Zero"/>
+          <Hour hour="Three"/>
+          <Hour hour="Six"/>
+          <Hour hour="Nine"/>
+          <Hour hour="Twelve"/>
+          <Hour hour="Fifteen"/>
+          <Hour hour="Eighteen"/>
+          <Hour hour="Twenty-one"/>
+          
+          <Hour hour="Zero"/>
+          <Hour hour="Three"/>
+          <Hour hour="Six"/>
+          <Hour hour="Nine"/>
+          <Hour hour="Twelve"/>
+          <Hour hour="Fifteen"/>
+          <Hour hour="Eighteen"/>
+          <Hour hour="Twenty-one"/><Hour hour="Zero"/>
+          <Hour hour="Three"/>
+          <Hour hour="Six"/>
+          <Hour hour="Nine"/>
+          <Hour hour="Twelve"/>
+          <Hour hour="Fifteen"/>
+          <Hour hour="Eighteen"/>
+          <Hour hour="Twenty-one"/><Hour hour="Zero"/>
+          <Hour hour="Three"/>
+          <Hour hour="Six"/>
+          <Hour hour="Nine"/>
+          <Hour hour="Twelve"/>
+          <Hour hour="Fifteen"/>
+          <Hour hour="Eighteen"/>
+          <Hour hour="Twenty-one"/>
+        </div>
       </div>
-      <div className="hours container">
-        <Hour hour="Zero"/>
-        <Hour hour="Three"/>
-        <Hour hour="Six"/>
-        <Hour hour="Nine"/>
-        <Hour hour="Twelve"/>
-        <Hour hour="Fifteen"/>
-        <Hour hour="Eighteen"/>
-        <Hour hour="Twenty-one"/>
-      </div>
+      
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,30 @@ import "./Header.css"
 
 export default function Header() {
 	return (
-		<>
-			<img id="header-logo" src="/src/assets/weather-app-icon.png" alt="App Icon" />
-		</>
+			<div id="header-container">
+				<div className="header ">
+					
+					<a className="left-side" href="#" >
+							<img className="middle-icon" id="header-logo" src="/src/assets/weather-app-icon.png" alt="App Icon" />
+							<p className="title">My Weather App</p>
+					</a>
+					
+					<input id="searchbar" className="" placeholder="Search..." required ></input>
+					
+					<div className="right-side">
+						<a href="https://www.instagram.com/zly.reper.zenyk/">
+							<img alt="Instagram Page" src="/src/assets/instagram.icon.svg" className="inst small-icon" />
+						</a>
+						
+						<a id="git" href="https://github.com/mryshkov">
+							<img alt="Github account" src="/src/assets/github.icon.svg" className="github small-icon" />
+						</a>
+						
+						<a href="https://t.me/watermeland">
+							<img alt="Telegram channel" src="/src/assets/telegram.icon.svg" className="telegram small-icon" />
+						</a>
+					</div>
+				</div>
+			</div>
 	)
 }
